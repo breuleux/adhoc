82e242a7bc18d593412ed2f9b91c66d76b4b66e2
Olivier Breuleux <breuleux@gmail.com>
2014-01-23 21:43:23 -0500

    new python version

--- workers/python.py
+++ workers/python.py
@@ -4,6 +4,24 @@ import sys
 import os
 import re
 
+def soft_newline(x):
+    if isinstance(x, str) and x.endswith("\n"):
+        x = x[:-1]
+    print x
+
+def pr(orig, x, do):
+    if isinstance(x, str):
+        do(x)
+    else:
+        try:
+            for line in x:
+                do(line)
+        except TypeError as e: # not iterable
+            if x is True:
+                do(orig)
+            elif x not in (None, False):
+                do(str(x))
+
 def _open(f):
     if isinstance(f, str):
         try:
@@ -14,65 +32,58 @@ def _open(f):
     else:
         return f
 
-def soft_newline(x):
-    if isinstance(x, str) and x.endswith("\n"):
-        x = x[:-1]
-    print x
-
-def pr(orig, x):
-    if isinstance(x, str):
-        soft_newline(x)
-    else:
-        try:
-            for line in x:
-                soft_newline(line)
-        except TypeError as e: # not iterable
-            if x is True:
-                soft_newline(orig)
-            elif x not in (None, False):
-                soft_newline(str(x))
-
-def do_whole(command, filename, f):
-    """
-    Defines: text (string)
-             filename
-    """
-    text = f.read()
-    pr(text, eval(command))
-
-def do_lines(command, filename, f):
-    """
-    Defines: xs (array of lines)
-             filename
-    """
-    xs = f.readlines()
-    pr(xs, eval(command))
-
-def do_line(command, filename, f):
+def do_line(command, filename, f, do):
     """
     Defines: x (current line)
              w (list of words)
              filename
     """
-    for x in f.readlines():
+    for line in f.readlines():
+        x = line
         w = x.split()
-        pr(x, eval(command))
+        pr(line, eval(command), do)
 
-def main(command, files):
-    for filename in files:
-        with _open(filename) as f:
-            if re.findall(r'\btext\b', command):
-                do_whole(command, filename, f)
-            elif re.findall(r'\bxs\b', command):
-                do_lines(command, filename, f)
-            else:
-                do_line(command, filename, f)
+def main(line_command, lines_commands, files):
+    if not line_command:
+        line_command = 'line'
+    if not lines_commands:
+        for filename in files:
+            with _open(filename) as f:
+                do_line(line_command, filename, f, soft_newline)
+    else:
+        lines = []
+        for filename in files:
+            with _open(filename) as f:
+                do_line(line_command, filename, f, lines.append)
+        orig = None
+        for cmd in lines_commands:
+            orig = lines
+            lines = eval(cmd)
+        pr(orig, lines, soft_newline)
+
+
 
 if __name__ == '__main__':
-    command = sys.argv[1]
-    files = sys.argv[2:]
-    if files:
-        main(command, files)
+    line_command = None
+    lines_commands = []
+    args = []
+    for arg in sys.argv[1:]:
+        if arg.startswith(":"):
+            lines_commands.append(arg[1:])
+        elif arg.startswith("\:"):
+            args.append(arg[1:])
+        elif not lines_commands and line_command is None:
+            line_command = arg
+        else:
+            args.append(arg)
+    if not args:
+        args = [sys.stdin]
+
+    if ((not line_command or line_command in ["help", "-help", "--help"])
+        and not lines_commands):
+        print """
+HELP
+"""
     else:
-        main(command, [sys.stdin])
+        main(line_command, lines_commands, args)
 
